4eb35c088af27a45dadd1d51c8dbe65d3380a3f8
pythontest <50592309+mikimasn@users.noreply.github.com>
2022-05-14 11:54:31 +0200

    fix

--- index.js
+++ index.js
@@ -105,8 +105,12 @@ app.post("/send",(req,res)=>{
 });
 app.post("/sendembed",(req,res)=>{
     if(req.body.title&&req.body.color&&req.body.desc){
-        var fields = req.body.fields||[];
-        console.log(typeof(fields));
+        var fields = req.body.fields.split("},")||[];
+        for(i=0;i<fields.length;i++){
+            fields[i]+="}"
+            fields[i]=JSON.parse(fields[i])
+        }
+            
         var embed = new MessageEmbed()
         .setTitle(req.body.title)
         .setColor(req.body.color)
